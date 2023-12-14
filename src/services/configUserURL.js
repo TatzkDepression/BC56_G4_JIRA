c7a3d14895ec05694bc1fbcf89a229efd9a377d5
thanhbinh bui <this.is.binhhh@gmail.com>
2023-12-14 18:54:19 +0700

    user

--- src/services/configUserURL.js
+++ src/services/configUserURL.js
@@ -4,11 +4,17 @@ import { LOCAL_SERVICE } from "./localService";
 const BASE_URL = "https://jiranew.cybersoft.edu.vn";
 
 const BASE_USER_URL = `${BASE_URL}/api/Users/`;
+const BASE_TASK_TYPE_URL = `${BASE_URL}/api/TaskType/`;
+const BASE_STATUS_URL = `${BASE_URL}/api/Status/`;
+const BASE_PROJECT_CATEGORY_URL = `${BASE_URL}/api/ProjectCategory`;
+const BASE_PROJECT_URL = `${BASE_URL}/api/Project/`;
+const BASE_PRIORITY_URL = `${BASE_URL}/api/Priority/`;
+const BASE_COMMENT_URL = `${BASE_URL}/api/Comment/`;
 
 const TOKEN_CYBERSOFT =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA1NiIsIkhldEhhblN0cmluZyI6IjE4LzA0LzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcxMzM5ODQwMDAwMCIsIm5iZiI6MTY4MzMwNjAwMCwiZXhwIjoxNzEzNTQ2MDAwfQ.4A7jJib1YUkmnIr-QDcjs_3j1YY0Ft1wPZDfe8qFrqE";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJOb2RlanMgMjgiLCJIZXRIYW5TdHJpbmciOiIwNi8wOC8yMDIzIiwiSGV0SGFuVGltZSI6IjE2OTEyODAwMDAwMDAiLCJuYmYiOjE2NzI5MzgwMDAsImV4cCI6MTY5MTQyNzYwMH0.1IXShq-PS4U5xC7QUMQLQcPPHNDmZrXfqvEBkJOEvEw";
 
-const AXIOS_INSTANCE_GENERATOR = (BASE_URL, accessToken = true) => {
+const AXIOS_INSTANCE_GENERATOR = (BASE_URL, accessToken = false) => {
     let config = {
         baseURL: BASE_URL,
         headers: {
@@ -16,6 +22,7 @@ const AXIOS_INSTANCE_GENERATOR = (BASE_URL, accessToken = true) => {
             Authorization: "Bearer " + LOCAL_SERVICE.user.get()?.accessToken,
         },
     };
+
     return axios.create(config);
 };
 
@@ -23,4 +30,9 @@ export {
     TOKEN_CYBERSOFT,
     AXIOS_INSTANCE_GENERATOR,
     BASE_USER_URL,
+    BASE_TASK_TYPE_URL,
+    BASE_STATUS_URL,
+    BASE_PRIORITY_URL,
+    BASE_PROJECT_URL,
+    BASE_PROJECT_CATEGORY_URL
 };
