509c67226efae0d73a13042b2fdbea102fc055ca
thanhbinh bui <this.is.binhhh@gmail.com>
2023-12-14 18:42:33 +0700

    user

--- src/services/configUserURL.js
+++ src/services/configUserURL.js
@@ -8,7 +8,7 @@ const BASE_USER_URL = `${BASE_URL}/api/Users/`;
 const TOKEN_CYBERSOFT =
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA1NiIsIkhldEhhblN0cmluZyI6IjE4LzA0LzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcxMzM5ODQwMDAwMCIsIm5iZiI6MTY4MzMwNjAwMCwiZXhwIjoxNzEzNTQ2MDAwfQ.4A7jJib1YUkmnIr-QDcjs_3j1YY0Ft1wPZDfe8qFrqE";
 
-const AXIOS_INSTANCE_GENERATOR = (BASE_URL, accessToken = false) => {
+const AXIOS_INSTANCE_GENERATOR = (BASE_URL, accessToken = true) => {
     let config = {
         baseURL: BASE_URL,
         headers: {
